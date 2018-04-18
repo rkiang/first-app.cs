95f4dfbd0ea776a6d3ecea5b83d3e1866c6a3b81
rkiang <ryanlkiang@gmail.com>
2018-04-18 15:52:41 -0500

    first commit, infinite loop

---
+++ firstapp.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace firstapp.cs
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+    namespace Loops
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                for (; ; )
+                {
+                    Console.WriteLine("Hey! I am Trapped");
+                }
+            }
+        }
+    }
+}
