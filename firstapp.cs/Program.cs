13efe42b8c8b80c4b17a21a76b845b983eca88ed
rkiang <ryanlkiang@gmail.com>
2018-04-20 14:41:30 -0500

    added int yrsXP to convert to a integer

--- firstapp.cs/Program.cs
+++ firstapp.cs/Program.cs
@@ -21,6 +21,23 @@ namespace firstapp.cs
                 yourName = Console.ReadLine();
 
                 Console.WriteLine("Hello {0}", yourName);
+
+                Console.WriteLine("How many years of coding have you done?");
+
+                int yrsXP = Convert.ToInt32(Console.ReadLine());
+
+                if (yrsXP > 10)
+                {
+                    Console.WriteLine("Wow you must be an expert!");
+                }
+                else if (yrsXP > 2)
+                {
+                    Console.WriteLine("Good, I can learn from you.");
+                }
+                else
+                {
+                    Console.WriteLine("I'm probably at your level.");
+                }
             }
         }
     }
